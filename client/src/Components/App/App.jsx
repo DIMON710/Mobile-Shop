f15ce7fd9cb083c6583c20e2c32e19b6115b5e2f
DIMON710 <55649421+DIMON710@users.noreply.github.com>
2023-09-28 22:52:28 +0300

    fix-router

--- client/src/Components/App/App.jsx
+++ client/src/Components/App/App.jsx
@@ -2,16 +2,16 @@ import React, {useState} from 'react';
 import '../../Components/App/App.css'
 import AppRouter from "./AppRouter.jsx";
 import {AdminComponent, Products} from "../../context/index.jsx";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 const App = () => {
     const [productValue, setProductValue] = useState([]);
     return (
         <div className="App">
             <Products.Provider value={[productValue, setProductValue]}>
                 <AdminComponent>
-                    <BrowserRouter>
+                    <HashRouter>
                         <AppRouter/>
-                    </BrowserRouter>
+                    </HashRouter>
                 </AdminComponent>
             </Products.Provider>
         </div>
